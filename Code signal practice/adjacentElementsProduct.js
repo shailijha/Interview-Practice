4694c1df78ec3cf3ff4f3aad68b2652de14b871b
shailijha <shaili_jha@yahoo.co.in>
2020-04-02 15:51:41 -0400

    Misc changes

--- Code signal practice/adjacentElementsProduct.js
+++
@@ -1,8 +0,0 @@
-function adjacentElementsProduct(inputArray) {
-  let largest_product = inputArray[0] * inputArray[1];
-  for (let i = 1; i < inputArray.length; i++) {
-    if (inputArray[i] * inputArray[i + 1] > largest_product)
-      largest_product = inputArray[i] * inputArray[i + 1];
-  }
-  return largest_product;
-}
