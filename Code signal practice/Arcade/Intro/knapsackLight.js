1cb8acbc48f0f80eee532d597d6db5ccb5b90c09
shailijha <shaili_jha@yahoo.co.in>
2020-04-03 01:09:53 -0400

    knapsack solution

---
+++ Code signal practice/Arcade/Intro/knapsackLight.js
@@ -0,0 +1,10 @@
+function knapsackLight(value1, weight1, value2, weight2, maxW) {
+  if (weight1 > maxW && weight2 > maxW) return 0;
+  else if (weight1 + weight2 == maxW || weight1 + weight2 < maxW)
+    return value1 + value2;
+  else if (weight1 == maxW && weight2 > maxW) return value1;
+  else if (weight2 == maxW && weight1 > maxW) return value2;
+  else {
+    return value1 > value2 ? value1 : value2;
+  }
+}
