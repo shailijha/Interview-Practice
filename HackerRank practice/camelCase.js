6845731588ff799e3d9f77957fc73a9ad44d6311
shailijha <shaili_jha@yahoo.co.in>
2020-04-09 23:45:15 -0400

    camel case algo soln

---
+++ HackerRank practice/camelCase.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', (_) => {
+  inputString = inputString
+    .replace(/\s*$/, '')
+    .split('\n')
+    .map((str) => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the camelcase function below.
+function camelcase(s) {
+  let regex = /[A-Z]/;
+  return s.split(regex).length;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  let result = camelcase(s);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
